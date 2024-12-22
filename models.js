3696193efe256282cf4661a837f5335ac65bc187
tmoubre <160032580+tmoubre@users.noreply.github.com>
2024-12-22 08:56:11 -0600

    updated hash password function

--- models.js
+++ models.js
@@ -30,7 +30,7 @@ userSchema.statics.hashPassword = (password) => {
 };
 
 userSchema.methods.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.Password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
