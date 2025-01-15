c26014b6230f08a8b6b4604faf3e7b4ba8aafcf9
tmoubre <160032580+tmoubre@users.noreply.github.com>
2025-01-15 11:43:27 -0600

    debub

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 const cors = require('cors');
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connectmongoose.connect(
+  "mongodb+srv://myFlixDBAdmin:16345antietam@cluster0.efoid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+  );
 app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
