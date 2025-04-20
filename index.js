5c6d2c0bcae902ee536911d0c2fe0a14b6e27ab2
tmoubre <160032580+tmoubre@users.noreply.github.com>
2025-04-20 15:34:28 -0500

    Updated Cors

--- index.js
+++ index.js
@@ -20,16 +20,17 @@ const path = require("path");
 let allowedOrigins = ['https://sci-fi-movies.netlify.app'];
 
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+// {
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 //Add Users
 app.post('/users',
